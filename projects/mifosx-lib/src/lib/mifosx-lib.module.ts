f252cd3b71dab8c44b62dd9d73ee68b28e18d709
Pushpendra <76156941+PC-11-00@users.noreply.github.com>
2024-06-06 16:51:54 +0530

    upgraded to angular-14

---
+++ projects/mifosx-lib/src/lib/mifosx-lib.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { MifosxLibComponent } from './mifosx-lib.component';
+
+
+
+@NgModule({
+  declarations: [
+    MifosxLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    MifosxLibComponent
+  ]
+})
+export class MifosxLibModule { }
